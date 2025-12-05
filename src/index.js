fb5c7dc8bce7d21773745678f1bb78e04d54bcea
Alphonso Gillis <shadowtheguy912@gmail.com>
2025-12-04 20:18:22 -0600

    Forgot to make the github

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import supabase from "./supabase.js";
+
+const app = express();
+app.use(express.json());
+
+app.get("/videogames", async (req, res) => {
+  const { data, error } = await supabase.from("Video Games").select("*");
+
+  if (error) {
+    return res.status(500).json({ error: error.message });
+  }
+
+  res.status(200).json(data);
+});
+
+app.post("/videogames", async (req, res) => {
+  const { name, creator, description, tags, price } = req.body
+
+  const newGame = {
+    name,
+    creator,
+    description,
+    tags,
+    price
+  }
+
+  const { data, error } = await supabase
+    .from("Video Games")
+    .insert(newGame)
+    .select();
+
+  if (error) {
+    return res.status(500).json({ error: error.message });
+  }
+
+  res.status(201).json(data);
+});
